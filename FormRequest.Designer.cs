c7cf8681990e8ee4a2fb14631981baa7d0c96a3d
qqniqq <mstitel2025@gmail.com>
2025-11-14 10:22:09 +0300

    Добавил форму для разработки задачи 2. реализация раздела заявки

---
+++ FormRequest.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Hospital
+{
+    partial class FormRequest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstRequests = new System.Windows.Forms.ListBox();
+            this.dgvRequestDetails = new System.Windows.Forms.DataGridView();
+            this.lblInfo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRequestDetails)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lstRequests
+            // 
+            this.lstRequests.FormattingEnabled = true;
+            this.lstRequests.Location = new System.Drawing.Point(13, 13);
+            this.lstRequests.Name = "lstRequests";
+            this.lstRequests.Size = new System.Drawing.Size(190, 95);
+            this.lstRequests.TabIndex = 0;
+            // 
+            // dgvRequestDetails
+            // 
+            this.dgvRequestDetails.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRequestDetails.Location = new System.Drawing.Point(12, 124);
+            this.dgvRequestDetails.Name = "dgvRequestDetails";
+            this.dgvRequestDetails.Size = new System.Drawing.Size(396, 286);
+            this.dgvRequestDetails.TabIndex = 1;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(449, 124);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(35, 13);
+            this.lblInfo.TabIndex = 2;
+            this.lblInfo.Text = "label1";
+            // 
+            // FormRequest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 422);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.dgvRequestDetails);
+            this.Controls.Add(this.lstRequests);
+            this.Name = "FormRequest";
+            this.Text = "FormRequest";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRequestDetails)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstRequests;
+        private System.Windows.Forms.DataGridView dgvRequestDetails;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
