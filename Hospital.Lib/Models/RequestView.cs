d8a1f7db5aa5795acc34cb204b3f3834200e7fd3
qqniqq <mstitel2025@gmail.com>
2025-11-15 09:24:38 +0300

    Добавил модель View

--- Hospital.Lib/Models/RequestView.cs
+++ Hospital.Lib/Models/RequestView.cs
@@ -1,12 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Hospital.Lib.Models
 {
-    internal class RequestView
+    public class RequestView
     {
+        public string Id { get; set; }
+        public string DepartmentName { get; set; }
+        public string DrugName { get; set; }
+        public int Quantity { get; set; }
+        public DateTime RequestDate { get; set; }
+        public string Status { get; set; }
+        public string ApprovedBy { get; set; }
+        public string Comment { get; set; }
     }
 }
