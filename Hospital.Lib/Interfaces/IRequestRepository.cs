c362ccd80fa585de0f0017ff6f22ad7d9f5b6c75
qqniqq <mstitel2025@gmail.com>
2025-11-15 09:26:29 +0300

    Добавил интерфейс репозитория

--- Hospital.Lib/Interfaces/IRequestRepository.cs
+++ Hospital.Lib/Interfaces/IRequestRepository.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Hospital.Lib.Models;
 
 namespace Hospital.Lib.Interfaces
 {
-    internal interface IRequestRepository
+    public interface IRequestRepository
     {
+        List<RequestView> GetAllRequests();
+        List<RequestView> GetRequestsById(string requestId);
     }
 }
